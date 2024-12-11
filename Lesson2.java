40ea8a5effa54813564934a1bbb51c3216d0ce9d
SmirnoffAI <smirnoff.coding@gmail.com>
2024-12-11 14:12:02 +0800

    .

---
+++ Lesson2.java
@@ -0,0 +1,71 @@
+public class Lesson2 {
+    public static void main(String[] args){
+        byte b_dec = 12;
+        byte b_bin = 0b1100;
+        byte b_oct = 014;
+        byte b_hex = 0xC;
+
+        short s_dec = -1300;
+        short s_bin = -0b010100010100;
+        short s_oct = -02424;
+        short s_hex = -0x514;
+
+        int i_dec = 0;
+        int i_bin = 0;
+        int i_oct = 0;
+        int i_hex = 0;
+
+        long l_dec = 123456789L;
+        long l_bin = 0b111010110111100110100010101L;
+        long l_oct = 0726746425L;
+        long l_hex = 0x75BCD15L;
+
+        float f1 = 5.5F;
+        float f2 = 3.14F;
+
+        double d1 = 3.11;
+        double d2 = 3.12;
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        char c1 = '1';
+        char c2 = 'b';
+        char c3 = 500;
+        char c4 = '\u0500';
+
+        System.out.println(b_dec);
+        System.out.println(b_bin);
+        System.out.println(b_oct);
+        System.out.println(b_hex);
+        System.out.println();
+        System.out.println(s_dec);
+        System.out.println(s_oct);
+        System.out.println(s_oct);
+        System.out.println(s_hex);
+        System.out.println();
+        System.out.println(i_dec);
+        System.out.println(i_bin);
+        System.out.println(i_oct);
+        System.out.println(i_hex);
+        System.out.println();
+        System.out.println(l_dec);
+        System.out.println(l_bin);
+        System.out.println(l_oct);
+        System.out.println(l_hex);
+        System.out.println();
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println();
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println();
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println();
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+    }
+}
